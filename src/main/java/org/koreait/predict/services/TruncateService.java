35bc386eac45108f2e3d0a8bdac282d0bf3082b3
yonggyo1125 <yonggyo00@kakao.com>
2025-07-03 14:46:56 +0900

    .

--- src/main/java/org/koreait/predict/services/TruncateService.java
+++ src/main/java/org/koreait/predict/services/TruncateService.java
@@ -3,6 +3,7 @@ package org.koreait.predict.services;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.configs.FileProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -15,6 +16,7 @@ public class TruncateService {
 
     private final FileProperties properties;
 
+    @Scheduled(cron="0 0 0 * * *")
     public void process() {
         File dir = new File(properties.getPath());
         if (!dir.exists() || !dir.isDirectory()) return;
