d6ca1e29b33793fb0a6b1c08f1be68989332d7e3
yonggyo1125 <yonggyo00@kakao.com>
2025-07-03 11:47:38 +0900

    .

--- src/main/java/org/koreait/predict/controllers/PredictController.java
+++ src/main/java/org/koreait/predict/controllers/PredictController.java
@@ -1,19 +1,24 @@
 package org.koreait.predict.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.koreait.predict.services.PredictService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/quickdraw/predict")
 public class PredictController {
 
-    @PostMapping
-    public void predict(@RequestPart("image") MultipartFile image) {
+    private final PredictService service;
 
+    @PostMapping
+    public List<String[]> predict(@RequestPart("image") MultipartFile image) {
+        return service.process(image);
     }
 }
