ba88f9df183d414dde16591d1d1e2a676808a632
yonggyo1125 <yonggyo00@kakao.com>
2025-07-03 12:01:25 +0900

    .

--- src/main/java/org/koreait/predict/services/PredictService.java
+++ src/main/java/org/koreait/predict/services/PredictService.java
@@ -7,6 +7,7 @@ import org.koreait.global.configs.PythonProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -24,6 +25,7 @@ public class PredictService {
 
     private final FileProperties fileProperties;
     private final PythonProperties pythonProperties;
+    private final WebApplicationContext ctx;
 
     public List<String[]> process(MultipartFile image) {
 
@@ -35,11 +37,11 @@ public class PredictService {
 
             String activationCommand = null, pythonPath = null;
             if (isProduction) { // 리눅스 환경, 서비스 환경
-                activationCommand = String.format("%s/activate", properties.getBase());
-                pythonPath = properties.getBase() + "/python";
+                activationCommand = String.format("%s/activate", pythonProperties.getBase());
+                pythonPath = pythonProperties.getBase() + "/python";
             } else { // 윈도우즈 환경
-                activationCommand = String.format("%s/activate.bat", properties.getBase());
-                pythonPath = properties.getBase() + "/python.exe";
+                activationCommand = String.format("%s/activate.bat", pythonProperties.getBase());
+                pythonPath = pythonProperties.getBase() + "/python.exe";
             }
 
             ProcessBuilder builder = isProduction ? new ProcessBuilder("/bin/sh", activationCommand) : new ProcessBuilder(activationCommand); // 가상환경 활성화
