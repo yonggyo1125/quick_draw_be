319009e9eb3335ebdccd56a90e56999cc1e28200
yonggyo1125 <yonggyo00@kakao.com>
2025-07-03 12:21:44 +0900

    .

---
+++ src/test/java/org/koreait/predict/services/PredictServiceTest.java
@@ -0,0 +1,26 @@
+package org.koreait.predict.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+@SpringBootTest
+public class PredictServiceTest {
+
+    @Autowired
+    private PredictService service;
+
+
+    @Test
+    void test() throws Exception {
+        InputStream in = new BufferedInputStream(new FileInputStream("C:/quickdraw/sample.jpg"));
+
+        MockMultipartFile file = new MockMultipartFile("file", "sample.jpg", "image/jpeg", in);
+
+    }
+}
